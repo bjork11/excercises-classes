b92ee46dd037b3f0f29a60eb680381915061c125
Adam Björk <adamhbjork@gmail.com>
2019-09-30 14:30:42 +0200

    Hund skapas och metoder provas

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace excercises_classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog minHund = new Dog();
+            minHund.name = "Nova";
+            minHund.age = 8;
+            minHund.tired = true;
+            minHund.color = "svart";
+            minHund.race = "En jävla massa aka blandras";
+
+            minHund.Fetch();
+            minHund.Bark();
+
+            Console.WriteLine(minHund.name + " är " + minHund.age + " år gammal.");
+            Console.WriteLine("Ras: " + minHund.race + "\nFärg: " + minHund.color);
+        }
+    }
+}
