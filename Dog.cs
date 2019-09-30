1ee1f112bc276c666edd23f1b0b3e8a964b1a3c2
Adam Björk <adamhbjork@gmail.com>
2019-09-30 14:30:08 +0200

    En hund

---
+++ Dog.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace excercises_classes
+{
+    public class Dog
+    {
+        public string name;
+        public int age;
+        public string race;
+        public string color;
+
+        public bool tired;
+
+        public void Fetch()
+        {
+            if (tired)
+            {
+                Console.WriteLine("Nej jag orkar inte hämta bollen, jag är trött.");
+            }
+            else
+            {
+                Console.WriteLine("BOOOOOOOOOOOOOOOLL!!");
+            }
+        }
+
+        public void Bark()
+        {
+            if (age < 1)
+            {
+                Console.WriteLine("Hunden är för liten för att skälla!");
+            }
+            else
+            {
+                Console.WriteLine("VOFF VOFF!");
+            }
+        }
+    }
+}
